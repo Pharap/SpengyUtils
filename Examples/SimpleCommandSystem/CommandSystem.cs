27732d9c0a3d195e95dcf335fbd395b8fe5ba0c9
Pharap <pharap@hotmail.com>
2017-09-10 18:01:42 +0100

    Update CommandSystem.cs
    * Added features
    * Adjusted formatting
    * Added new commands

--- Examples/SimpleCommandSystem/CommandSystem.cs
+++ Examples/SimpleCommandSystem/CommandSystem.cs
@@ -1,14 +1,44 @@
+private const bool echoLog = true;
 
 public void Main(string argument)
 {
 	Command command = new Command();
-	
-	if(!Command.TryParse(argument, ref command))
+	if(string.IsNullOrWhiteSpace(argument))
 	{
-		return;
+		if(echoLog)
+		{
+			Echo("Empty Argument");
+		}
 	}
-	
-	Process(command);
+	else if(!Command.TryParse(argument, ref command))
+	{
+		if(echoLog)
+		{
+			Echo("Failed Parse:");
+			Echo(argument);
+		}
+	}
+	else
+	{
+		try
+		{
+			var result = Process(command);
+			if(echoLog)
+			{
+				Echo(result ? "Success" : "Failure");
+			}
+		}
+		catch(CommandException ex)
+		{
+			Echo(ex.Message);
+		}
+		catch(Exception ex)
+		{
+			// If this block is ever reached
+			// Notify the developer
+			throw ex;
+		}
+	}
 }
 
 delegate bool GroupFunction(IMyBlockGroup group);
@@ -16,35 +46,71 @@ delegate bool BlockFunction(IMyTerminalBlock block);
 
 static readonly Dictionary<string, BlockFunction> blockFunctions = new Dictionary<string, BlockFunction>()
 {
-	{ MakeIndex("door", "open"), MakeBlockFunction((IMyDoor d) => d.OpenDoor()) },
-	{ MakeIndex("door", "close"), MakeBlockFunction((IMyDoor d) => d.CloseDoor()) },
-	{ MakeIndex("door", "toggle"), MakeBlockFunction((IMyDoor d) => d.ToggleDoor()) },
-	{ MakeIndex("piston", "extend"), MakeBlockFunction((IMyPistonBase p) => p.Extend()) },
-	{ MakeIndex("piston", "retract"), MakeBlockFunction((IMyPistonBase p) => p.Retract()) },
-	{ MakeIndex("piston", "reverse"), MakeBlockFunction((IMyPistonBase p) => p.Reverse()) },
-	{ MakeIndex("functional", "enable"), MakeBlockFunction((IMyFunctionalBlock f) => f.Enabled = true) },
-	{ MakeIndex("functional", "disable"), MakeBlockFunction((IMyFunctionalBlock f) => f.Enabled = false) },
-	{ MakeIndex("remotecontrol", "enable"), MakeBlockFunction((IMyRemoteControl r) => r.SetAutoPilotEnabled(true)) },
-	{ MakeIndex("remotecontrol", "disable"), MakeBlockFunction((IMyRemoteControl r) => r.SetAutoPilotEnabled(false)) },
+	// Door Commands
+	{ MakeIndex("Door", "Open"),   MakeBlockFunction((IMyDoor d) => d.OpenDoor())   },
+	{ MakeIndex("Door", "Close"),  MakeBlockFunction((IMyDoor d) => d.CloseDoor())  },
+	{ MakeIndex("Door", "Toggle"), MakeBlockFunction((IMyDoor d) => d.ToggleDoor()) },
+	// Piston Commands
+	{ MakeIndex("Piston", "Extend"),  MakeBlockFunction((IMyPistonBase p) => p.Extend())  },
+	{ MakeIndex("Piston", "Retract"), MakeBlockFunction((IMyPistonBase p) => p.Retract()) },
+	{ MakeIndex("Piston", "Reverse"), MakeBlockFunction((IMyPistonBase p) => p.Reverse()) },
+	// Functional Commands
+	{ MakeIndex("Functional", "Enable"),  MakeBlockFunction((IMyFunctionalBlock f) => f.Enabled = true)  },
+	{ MakeIndex("Functional", "Disable"), MakeBlockFunction((IMyFunctionalBlock f) => f.Enabled = false) },
+	// Remote Control Commands
+	{ MakeIndex("RemoteControl", "EnableAutoPilot"),  MakeBlockFunction((IMyRemoteControl r) => r.SetAutoPilotEnabled(true))  },
+	{ MakeIndex("RemoteControl", "DisableAutoPilot"), MakeBlockFunction((IMyRemoteControl r) => r.SetAutoPilotEnabled(false)) },
+	// Antenna Commands
+	{ MakeIndex("Antenna", "EnableAlliedBroadcast"),  MakeBlockFunction<IMyRadioAntenna>(a => a.IgnoreAlliedBroadcast = false) },
+	{ MakeIndex("Antenna", "DisableAlliedBroadcast"), MakeBlockFunction<IMyRadioAntenna>(a => a.IgnoreAlliedBroadcast = true)  },
+	{ MakeIndex("Antenna", "EnableOtherBroadcast"),   MakeBlockFunction<IMyRadioAntenna>(a => a.IgnoreOtherBroadcast = false)  },
+	{ MakeIndex("Antenna", "DisableOtherBroadcast"),  MakeBlockFunction<IMyRadioAntenna>(a => a.IgnoreOtherBroadcast = true)   },
+	// Connector Commands
+	{ MakeIndex("Connector", "EnableThrowOut"),    MakeBlockFunction<IMyShipConnector>(c => c.ThrowOut = true)    },
+	{ MakeIndex("Connector", "DisableThrowOut"),   MakeBlockFunction<IMyShipConnector>(c => c.ThrowOut = false)   },
+	{ MakeIndex("Connector", "EnableCollectAll"),  MakeBlockFunction<IMyShipConnector>(c => c.CollectAll = true)  },
+	{ MakeIndex("Connector", "DisableCollectAll"), MakeBlockFunction<IMyShipConnector>(c => c.CollectAll = false) },
+	{ MakeIndex("Connector", "Connect"),           MakeBlockFunction<IMyShipConnector>(c => c.Connect())          },
+	{ MakeIndex("Connector", "Disconnect"),        MakeBlockFunction<IMyShipConnector>(c => c.Disconnect())       },
+	{ MakeIndex("Connector", "ToggleConnect"),     MakeBlockFunction<IMyShipConnector>(c => c.ToggleConnect())    },
 };
 	
 static readonly List<IMyDoor> doors = new List<IMyDoor>();
 static readonly List<IMyPistonBase> pistons = new List<IMyPistonBase>();
 static readonly List<IMyFunctionalBlock> functionalBlocks = new List<IMyFunctionalBlock>();
 static readonly List<IMyRemoteControl> remoteControls = new List<IMyRemoteControl>();
+static readonly List<IMyRadioAntenna> antennas = new List<IMyRadioAntenna>();
+static readonly List<IMyShipConnector> connectors = new List<IMyShipConnector>();
 
 static readonly Dictionary<string, GroupFunction> groupFunctions = new Dictionary<string, GroupFunction>()
 {
-	{ MakeIndex("door", "open"), MakeGroupFunction(doors, d => d.OpenDoor()) },
-	{ MakeIndex("door", "close"), MakeGroupFunction(doors, d => d.CloseDoor()) },
-	{ MakeIndex("door", "toggle"), MakeGroupFunction(doors, d => d.ToggleDoor()) },
-	{ MakeIndex("piston", "extend"), MakeGroupFunction(pistons, p => p.Extend()) },
-	{ MakeIndex("piston", "retract"), MakeGroupFunction(pistons, p => p.Retract()) },
-	{ MakeIndex("piston", "reverse"), MakeGroupFunction(pistons, p => p.Reverse()) },
-	{ MakeIndex("functional", "enable"), MakeGroupFunction(functionalBlocks, f => f.Enabled = true) },
-	{ MakeIndex("functional", "disable"), MakeGroupFunction(functionalBlocks, f => f.Enabled = false) },
-	{ MakeIndex("remotecontrol", "enable"), MakeGroupFunction(remoteControls, r => r.SetAutoPilotEnabled(true)) },
-	{ MakeIndex("remotecontrol", "disable"), MakeGroupFunction(remoteControls, r => r.SetAutoPilotEnabled(false)) },
+	// Door Commands
+	{ MakeIndex("Door", "Open"),   MakeGroupFunction(doors, d => d.OpenDoor())   },
+	{ MakeIndex("Door", "Close"),  MakeGroupFunction(doors, d => d.CloseDoor())  },
+	{ MakeIndex("Door", "Toggle"), MakeGroupFunction(doors, d => d.ToggleDoor()) },
+	// Piston Commands
+	{ MakeIndex("Piston", "Extend"),  MakeGroupFunction(pistons, p => p.Extend())  },
+	{ MakeIndex("Piston", "Retract"), MakeGroupFunction(pistons, p => p.Retract()) },
+	{ MakeIndex("Piston", "Reverse"), MakeGroupFunction(pistons, p => p.Reverse()) },
+	// Functional Commands
+	{ MakeIndex("Functional", "Enable"),  MakeGroupFunction(functionalBlocks, f => f.Enabled = true)  },
+	{ MakeIndex("Functional", "Disable"), MakeGroupFunction(functionalBlocks, f => f.Enabled = false) },
+	// Remote Control Commands
+	{ MakeIndex("RemoteControl", "EnableAutoPilot"),  MakeGroupFunction(remoteControls, r => r.SetAutoPilotEnabled(true))  },
+	{ MakeIndex("RemoteControl", "DisableAutoPilot"), MakeGroupFunction(remoteControls, r => r.SetAutoPilotEnabled(false)) },
+	// Antenna Commands
+	{ MakeIndex("Antenna", "EnableAlliedBroadcast"),  MakeGroupFunction(antennas, a => a.IgnoreAlliedBroadcast = false) },
+	{ MakeIndex("Antenna", "DisableAlliedBroadcast"), MakeGroupFunction(antennas, a => a.IgnoreAlliedBroadcast = true)  },
+	{ MakeIndex("Antenna", "EnableOtherBroadcast"),   MakeGroupFunction(antennas, a => a.IgnoreOtherBroadcast = false)  },
+	{ MakeIndex("Antenna", "DisableOtherBroadcast"),  MakeGroupFunction(antennas, a => a.IgnoreOtherBroadcast = true)   },
+	// Connector Commands
+	{ MakeIndex("Connector", "EnableThrowOut"),    MakeGroupFunction(connectors, c => c.ThrowOut = true)    },
+	{ MakeIndex("Connector", "DisableThrowOut"),   MakeGroupFunction(connectors, c => c.ThrowOut = false)   },
+	{ MakeIndex("Connector", "EnableCollectAll"),  MakeGroupFunction(connectors, c => c.CollectAll = true)  },
+	{ MakeIndex("Connector", "DisableCollectAll"), MakeGroupFunction(connectors, c => c.CollectAll = false) },
+	{ MakeIndex("Connector", "Connect"),           MakeGroupFunction(connectors, c => c.Connect())          },
+	{ MakeIndex("Connector", "Disconnect"),        MakeGroupFunction(connectors, c => c.Disconnect())       },
+	{ MakeIndex("Connector", "ToggleConnect"),     MakeGroupFunction(connectors, c => c.ToggleConnect())    },
 };
 
 static string MakeIndex(string typeName, string actionName)
@@ -70,14 +136,14 @@ bool ProcessBlock(string name, string type, string action)
 	var block = GridTerminalSystem.GetBlockWithName(name);
 	
 	if(block == null)
-		return false;
+		throw new CommandException("Block not found:" + name);
 	
 	BlockFunction function;
 	var index = MakeIndex(type, action);
 	if(blockFunctions.TryGetValue(index, out function))
 		return function(block);
-	
-	return false;
+		
+	throw new CommandException("Could not find action \"" + action + "\" for block type " + type);
 }
 
 bool ProcessGroup(string name, string type, string action)
@@ -85,14 +151,15 @@ bool ProcessGroup(string name, string type, string action)
 	var group = GridTerminalSystem.GetBlockGroupWithName(name);
 	
 	if(group == null)
-		return false;
+		throw new CommandException("Group not found:" + name);
 	
 	GroupFunction function;
 	var index = MakeIndex(type, action);
 	if(groupFunctions.TryGetValue(index, out function))
+	{
 		return function(group);
-	
-	return false;
+	}	
+	throw new CommandException("Could not find action \"" + action + "\" for block type " + type);
 }
 
 static BlockFunction MakeBlockFunction<T>(Action<T> action)
@@ -119,9 +186,15 @@ static GroupFunction MakeGroupFunction<T>(List<T> list, Action<T> action) where 
 			
 		foreach(var item in list)
 			action(item);
+		list.Clear();
 			
 		return true;
 	};
+}
+
+public class CommandException : Exception
+{
+	public CommandException(string message) : base(message) {}
 }
 
 public enum Qualifier
@@ -141,6 +214,11 @@ public struct Command
 	public string Type { get { return this.type; } }
 	public string Action { get { return this.action; } }
 	public Qualifier Qualifier { get { return this.qualifier; } }
+	
+	public override string ToString()
+	{
+		return string.Format("{0} {1} {2} \"{3}\"", Action, Type, Qualifier, Name);
+	}
 		
 	public static bool TryParse(string input, ref Command command)
 	{		
@@ -175,7 +253,7 @@ public struct Command
 			
 		command = result;
 		return true;
-	}	
+	}
 }
 
 public class StringReader
@@ -234,9 +312,7 @@ public class StringReader
 				++this.index;
 				c = this.source[this.index];
 				if(c == '\n')
-				{
 					++this.index;
-				}
 				return this.source.Substring(start, end - start);
 			}
 			else if(c == '\n')
@@ -251,10 +327,7 @@ public class StringReader
 	
 	public string ReadToEnd()
 	{
-		return
-			(this.index > 0) ? 
-			this.source.Substring(index) :
-			this.source;
+		return (this.index > 0) ?  this.source.Substring(index) : this.source;
 	}
 }
 
