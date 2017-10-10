c77db02f43c7cf5fed481dfb001782bd7ef2b0f2
Pharap <pharap@hotmail.com>
2017-10-10 20:53:16 +0100

    Update TerminalBlockInspector.cs

--- Utils/TerminalBlockInspector.cs
+++ Utils/TerminalBlockInspector.cs
@@ -1,22 +1,24 @@
 // Lists all actions and properties of a terminal block
 
-public void Main(string argument)
-{
-    var block = GridTerminalSystem.GetBlockWithName(argument) as IMyTerminalBlock;
-
-    var builder = new StringBuilder();
-
-    var actions = new List<ITerminalAction>();
-    block.GetActions(actions);
-    foreach(var action in actions)
-        builder.AppendFormat("{0} ({1})\n", action.Id, action.Name);
-
-    builder.Append("\n");
-    
-    var properties = new List<ITerminalProperty>();
-    block.GetProperties(properties);
-    foreach(var prop in properties)
-        builder.AppendFormat("{0} : {1}\n", prop.Id, prop.TypeName);
-
-    Echo(builder.ToString());
+public void Main(string argument) 
+{ 
+    var block = GridTerminalSystem.GetBlockWithName(argument) as IMyTerminalBlock; 
+ 
+    var builder = new StringBuilder(); 
+ 
+    builder.AppendLine("Actions");
+    var actions = new List<ITerminalAction>(); 
+    block.GetActions(actions); 
+    foreach(var action in actions) 
+        builder.AppendFormat("{0} ({1})\n", action.Id, action.Name); 
+  
+    builder.AppendLine();
+      
+    builder.AppendLine("Properties");
+    var properties = new List<ITerminalProperty>(); 
+    block.GetProperties(properties); 
+    foreach(var prop in properties) 
+        builder.AppendFormat("{0} : {1}\n", prop.Id, prop.TypeName); 
+ 
+    Echo(builder.ToString()); 
 }
